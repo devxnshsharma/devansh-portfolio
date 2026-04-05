0b7d03e55294330d0d15334e21b61421835a300a
devansh <devanshsharma0363@gmail.com>
2026-04-05 20:21:44 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
   base: process.env.GITHUB_ACTIONS ? '/devansh-portfolio/' : '/',
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
   server: {
     host: "::",
     port: 8080,
